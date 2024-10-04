af3510cb733649a0f7a19b3e15558afdc0439d14
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2024-10-04 22:28:28 +0700

    fix shellchecker errors

--- solution/fizzbuzz.sh
+++ solution/fizzbuzz.sh
@@ -1,9 +1,11 @@
-for i in $(seq 1 $1); do
-    if [ $(($i % 3)) -eq 0 ] && [ $(($i % 5)) -eq 0 ]; then
+#!/bin/bash
+
+for i in $(seq 1 "$1"); do
+    if [ $((i % 3)) -eq 0 ] && [ $((i % 5)) -eq 0 ]; then
         echo -n "Fizz Buzz"
-    elif [ $(($i % 3)) -eq 0 ]; then
+    elif [ $((i % 3)) -eq 0 ]; then
         echo -n "Fizz"
-    elif [ $(($i % 5)) -eq 0 ]; then
+    elif [ $((i % 5)) -eq 0 ]; then
         echo -n "Buzz"
     else
         echo -n "$i"
